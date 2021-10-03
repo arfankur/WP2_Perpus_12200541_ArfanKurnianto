4c0f444bb1b81f72bc5a3c5535f36332e9c063c4
arfankur <arfankurnianto29@gmail.com>
2021-10-03 19:43:08 +0700

    final commit

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Arfan</li>
+        <li>Nama Belakang : Kurnianto</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Cilebut Barat </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Lagu Favorit</h4>
+    <ul type="square">
+        <li>Yellow Claw</li>
+    </ul>
+</section>
