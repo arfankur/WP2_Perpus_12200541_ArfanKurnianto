704efca87ca3b05aa0f188039b5fc6e730152660
arfankur <arfankurnianto29@gmail.com>
2021-10-09 10:14:49 +0700

    final commit

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -1,4 +1,5 @@
 <?php
+
 class Latihan1 extends CI_Controller
 {
     public function index()
@@ -8,7 +9,7 @@ class Latihan1 extends CI_Controller
     }
  public function penjumlahan($n1 = null, $n2 = null)
     {
-        $this->load->model('Model_latihan1');
+        // $this->load->model('Model_latihan1');
      $data['nilai1'] = $n1;
      $data['nilai2'] = $n2;
      $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
